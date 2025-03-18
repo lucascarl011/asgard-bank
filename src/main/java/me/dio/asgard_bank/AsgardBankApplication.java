bef6eba31e89d2403bed242c5d886fb5ad488d24
lucascarl011 <lucassscarlosss54@gmail.com>
2025-03-18 20:33:32 -0300

    Configurações iniciais

---
+++ src/main/java/me/dio/asgard_bank/AsgardBankApplication.java
@@ -0,0 +1,13 @@
+package me.dio.asgard_bank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsgardBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AsgardBankApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/dio/asgard_bank/AsgardBankApplicationTests.java
@@ -0,0 +1,13 @@
+package me.dio.asgard_bank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AsgardBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
